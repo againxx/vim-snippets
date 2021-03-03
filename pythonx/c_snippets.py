7c1332d325b2194c2b9958dd96177d528ee362fd
Xi Xia <againxx@mail.ustc.edu.cn>
2021-03-03 15:59:55 +0800

    Update c/cpp snippets for different brace styles

--- pythonx/c_snippets.py
+++ pythonx/c_snippets.py
@@ -1,18 +1,18 @@
 def get_left_curly_brace_style(snip):
-	style = snip.opt("b:ultisnips_cpp_style")
-	if style is None:
-		style = snip.opt("g:ultisnips_cpp_style", "google")
-	if style == "google":
-		snip.rv += " {"
-	else:
-		snip += snip.mkline("{")
+    style = snip.opt("b:ultisnips_cpp_style")
+    if style is None:
+        style = snip.opt("g:ultisnips_cpp_style", "google")
+    if style == "google":
+        snip.rv += " {"
+    else:
+        snip += snip.mkline("{")
 
 def get_right_curly_brace_style(snip, str_after_brace):
-	style = snip.opt("b:ultisnips_cpp_style")
-	if style is None:
-		style = snip.opt("g:ultisnips_cpp_style", "google")
-	if style == "google":
-		snip.rv += "} " + str_after_brace
-	else:
-		snip += snip.mkline(str_after_brace)
-
+    style = snip.opt("b:ultisnips_cpp_style")
+    if style is None:
+        style = snip.opt("g:ultisnips_cpp_style", "google")
+    if style == "google":
+        snip.rv += "} " + str_after_brace
+    else:
+        snip.rv += "}"
+        snip += snip.mkline(str_after_brace)
